efe92150563d4d2bcae6b60dadadd9be2c758421
alexandrecuer <alexandre.cuer@cerema.fr>
2020-05-26 09:27:24 +0200

    Update batiment.py
    quelques propositions de modifs :-)

--- examples/Batiment/batiment.py
+++ examples/Batiment/batiment.py
@@ -25,21 +25,33 @@ IMPORTATION DES DONNES METEOS (VARIABLES EN FONCTION DU TEMPS)
 5 : rayonnement global (en W/m2)
 6 : rayonnement atmospherique (en W/m2)
 """
-meteo = np.loadtxt('meteo3.txt')
-Tint=19
-Text=meteo[:,1]
-t=meteo[:,0]
+# tu peux directement charger le fichier déposé par Frédéric, pas la peine de le recopier dans le répertoire courant....
+# ../ veut dire que tu retournes un répertoire en arrière
+# donc içi avec ../../ tu retournes au niveau de la racine de ton dossier github
+meteo = np.loadtxt('../../datas/corr1_RT2012_H1c_toute_annee.txt')
+#meteo = np.loadtxt('meteo3.txt')
+# mieux vaut appeler cette variable Tconsigne, Tint pourrait servir pour héberger la température intérieure modélisée 
+Tconsigne=19
+#Tint=19
+# tu n'es pas obligé de créer un vecteur spécifique...celà mobilise de la mémoire en plus
+#Text=meteo[:,1]
+#t=meteo[:,0]
 Rm=8.24E-02 # Résistance thermique des murs (K/W)
 Ri=1.43E-03 # Résistance superficielle intérieure
 Rf=0.034 # Résistance due aux ifilitration et au renouvellement d'air
 Rthe=1/(1/(Rm+Ri)+1/Rf) # Résistance thermique équivalente 
 Ci=18.407 # Capacité thermique de l'air (Wh/K)
 Cm=2636 # Capacité thermique des murs 
-Besoin=(Tint-Text)/Rthe
+Besoin=(Tconsigne-meteo[:,1])/Rthe
 #Graphique
 figure1 = plt.figure(figsize = (10, 5))
+ax1=plt.subplot(111)
 plt.xlabel('Temps (en heure)')
 plt.ylabel('Puissance (W)')
 plt.title("Evolution des besoins d\'un bâtiment de 20m2 sur une année")
-plt.plot(t,Besoin)
+plt.plot(meteo[:,0],Besoin,label="besoin en Kwh",color="orange")
+plt.legend(loc="upper left")
+ax2=ax1.twinx()
+plt.plot(meteo[:,0],meteo[:,1],label="Text")
+plt.legend(loc="upper right")
 plt.show()
