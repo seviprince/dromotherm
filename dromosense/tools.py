128c00e2ce7e1193819f77afc539e8cff33765a4
seviprince <63263854+seviprince@users.noreply.github.com>
2020-05-26 13:19:10 +0200

    Update tools.py

--- dromosense/tools.py
+++ dromosense/tools.py
@@ -5,3 +5,12 @@ def rd(k1,k2,h1,h2):
     W/(m2K)
     """
     return 2*k1*k2/(h1*k2+h2*k1)
+
+def besoin_bat(Tconsigne,Text,Rm,Ri,Rf):
+    """
+    Calcul les besoins du bâtiment avec le modèle RC
+
+    
+    """
+    Rthe=1/(1/(Rm+Ri)+1/Rf)# Résistance thermique équivalente 
+    return (Tconsigne-Text)/Rthe
