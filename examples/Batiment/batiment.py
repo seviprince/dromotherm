18e03d8b3635d52bebf0d1a755b987f31dfe5405
seviprince <63263854+seviprince@users.noreply.github.com>
2020-05-26 12:22:42 +0200

    Update batiment.py

--- examples/Batiment/batiment.py
+++ examples/Batiment/batiment.py
@@ -2,40 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import cmath as math
-"""
-IMPORTATION DES TEMPERATURES DU MODELE 2D
-dans l'ordre
-colonne 0 : temps
-colonne 1 : température couche de surface
-colonne 2 : température couche drainante
-colonne 3 : température couche de base
-colonne 4 : température couche fictive ?
-colonne 5 : température massif
-"""
-
-
 
 """
 IMPORTATION DES DONNES METEOS (VARIABLES EN FONCTION DU TEMPS)
-0 : temps exprime en secondes depuis le 01/06 00:00
+0 : temps exprime en heure 
 1 : temperature d'air (en deg Celsius)
-2 : temperature du point de rosee (pas utile)
-3 : nature des precipitations (pas utile)
+2 : rayonnement global (en W/m2)
+3 : rayonnement atmospherique (en W/m2)
 4 : vitesse du vent (en m/s)
-5 : rayonnement global (en W/m2)
-6 : rayonnement atmospherique (en W/m2)
 """
+def besoin_bat(Tint,Text,Rm,Ri,Rf):
+    Rthe=1/(1/(Rm+Ri)+1/Rf)# Résistance thermique équivalente 
+    return (Tint-Text)/Rthe
 meteo = np.loadtxt('meteo3.txt')
 Tint=19
 Text=meteo[:,1]
 t=meteo[:,0]
+R=np.array(3)
+C=np.array(2)
 Rm=8.24E-02 # Résistance thermique des murs (K/W)
 Ri=1.43E-03 # Résistance superficielle intérieure
-Rf=0.034 # Résistance due aux ifilitration et au renouvellement d'air
-Rthe=1/(1/(Rm+Ri)+1/Rf) # Résistance thermique équivalente 
+Rf=0.034 # Résistance due aux infiltrations+vitre et au renouvellement d'air 
 Ci=18.407 # Capacité thermique de l'air (Wh/K)
 Cm=2636 # Capacité thermique des murs 
-Besoin=(Tint-Text)/Rthe
+Besoin= besoin_bat(Tint,Text,Rm,Ri,Rf)
 #Graphique
 figure1 = plt.figure(figsize = (10, 5))
 plt.xlabel('Temps (en heure)')
