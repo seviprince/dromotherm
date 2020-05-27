4b096be087e50ab7d699ae65ba9bfe654075bfb3
seviprince <63263854+seviprince@users.noreply.github.com>
2020-05-27 20:10:54 +0200

    Update tools.py

--- dromosense/tools.py
+++ dromosense/tools.py
@@ -1,3 +1,4 @@
+import numpy as np
 def rd(k1,k2,h1,h2):
     """
     calcule le coefficient d'échange surfacique entre 2 couches de conductivité k1 et k2 et d'épaisseurs h1 et h2
@@ -34,3 +35,26 @@ def besoin_bat(Tconsigne,Text,Rm,Ri,Rf):
 
     """
     return (Tconsigne-Text)*(1/(Rm+Ri)+1/Rf)
+
+def sol_tridiag(A,B,C,D):
+    """
+    Résout un système matriciel de la forme MX=D avec M une matrice 
+    tridiagonale ayant:
+    A: vecteur constituant la diagonale pricipale 
+    B: vecteur constituant la diagonale supérieure
+    C: le vecteur constituant la diagonale inférieure
+    """
+    N = A.size
+    alpha=np.zeros((N))
+    beta=np.zeros((N))
+    X=np.zeros((N))
+    alpha[0]=A[0]
+
+    beta[0]=D[0]/alpha[0]
+    for i in range(0,N-1):
+        alpha[i+1]=A[i+1]-(C[i+1]*B[i])/alpha[i]
+        beta[i+1]=(D[i+1]-B[i]*beta[i])/alpha[i+1]
+    X[N-1]=beta[N-1]
+    for i in range(N-2,-1,-1):
+        X[i]=beta[i]-(B[i]*X[i+1]/alpha[i])
+    return X
