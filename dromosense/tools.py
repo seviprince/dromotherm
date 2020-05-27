08bd82e7b54fe959e2961df1b342a90a2f72a2cf
seviprince <63263854+seviprince@users.noreply.github.com>
2020-05-27 11:56:54 +0200

    Update tools.py

--- dromosense/tools.py
+++ dromosense/tools.py
@@ -23,3 +23,23 @@ def besoin_bat(Tconsigne,Text,Rm,Ri,Rf):
     """
     Rthe=1/(1/(Rm+Ri)+1/Rf)# Résistance thermique équivalente 
     return (Tconsigne-Text)/Rthe
+
+def sol_tridiag(A,B,C,D):
+    """
+    Résout un système matriciel tridiagonal
+
+    """
+    N = A.size
+    alpha=np.zeros((N))
+    beta=np.zeros((N))
+    X=np.zeros((N))
+    alpha[0]=A[0]
+
+    beta[0]=D[0]/alpha[0]
+    for i in range(0,N-1):
+        alpha[i+1]=A[i+1]-(C[i+1]*B[i])/alpha[i]
+        beta[i+1]=(D[i+1]-B[i]*beta[i])/alpha[i+1]
+    X[N-1]=beta[N-1]
+    for i in range(N-2,-1,-1):
+        X[i]=beta[i]-(B[i]*X[i+1]/alpha[i])
+    return X
