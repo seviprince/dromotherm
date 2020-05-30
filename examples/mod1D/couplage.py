a1b9d12c0618f7e8d258e3870164deab48470fb9
alexandrecuer <alexandre.cuer@cerema.fr>
2020-05-30 19:16:41 +0200

    Update couplage.py

--- examples/mod1D/couplage.py
+++ examples/mod1D/couplage.py
@@ -27,39 +27,31 @@ input("press any key")
 def F(y,t):
     """
     Le stockage a 2 entrées et 2 sorties
-    Te1=Ts2 (entrée du fluide quittant le dromotherm vers le stockage)
-    
-    Te2=Température d'entrée de l'eau glycolée quittant la PAC pour le stockage
-    
-    Ts1=Tsorties_stockage(rho_eau*qf,Cpf,Te1,y,k_global)
     """
-    print(t)
+    print("we have t={} and y={}".format(t,y))
+    
     i=int(t)
-    Te1=Ts2_ech[i]
-    Te2=Te2_sto[i]
-    return (1/(m_sable*Cp_sable))*((m1*Cpf))*(Te1-Tsorties_stockage(rho_eau*qf,Cpf,Te1,y,k_global))+m2*C2*(Te2-Tsorties_stockage(m2,C2,Te2,y,k_global))
     
-def Tsorties_stockage(m,Cp,Te,Tsable,k):
-    """
-    expliquer
-    """
-    return (1/(m*Cp+0.5*k))*((m*Cp-0.5*k)*Te+k*Tsable)
+    Tsor_sto[i] = ( k * y - coeff * eff * Tsor_dro[i] ) / a
+    
+    Tinj_sto[i] = Tsor_sto[i] + coeff * eff * (Tsor_dro[i] - Tsor_sto[i])
+    
+    Tinj_dro[i] = Tsor_dro[i] - eff * (Tsor_dro[i] - Tsor_sto[i])
+    
+    return msto * cpsto * (Tinj_sto[i] - Tsor_sto[i]) / (m_sable * Cp_sable)
 
 
-Ts1_ech=np.zeros(meteo.shape[0]) # Ts1_ech=Te1_sto
-Ts2_ech=np.zeros(meteo.shape[0]) 
-Ts1_sto=10*np.ones(meteo.shape[0])
-Ts2_sto=np.zeros(meteo.shape[0])
-Te2_sto=np.zeros(meteo.shape[0])
-# Températures initailes de tout le système
-Ts1_ech[0]=10#+kelvin
-Ts2_ech[0]=10#+kelvin
-Ts1_sto[0]=10#+kelvin
-Ts2_sto[0]=10#+kelvin
-Te2_sto[0]=10#+kelvin
-# Température fluide circulant dans le dromotherm
-Tsf_dro=T[:,1]
+# température d'entrée et de sortie du fluide dans le stockage
+Tinj_sto=np.zeros(meteo.shape[0])
+# température de sortie du fluide après transit dans le stockage
+Tsor_sto=np.zeros(meteo.shape[0])
 
+# température d'injection du fluide dans le dromotherme
+Tinj_dro=10*np.ones(meteo.shape[0])
+# température de sortie de fluide après collecte de chaleur dans le dromotherme
+Tsor_dro=T[:,1]
+
+"""
 # DONNES SUR LE STOCKAGE
 eff=0.8 # efficacité de l'échangeur 
 q1=qf # débit du fluide circulant de l'échangeur vers le stockage
@@ -68,24 +60,63 @@ q2=0.004/60 # débit du fluide caloporteur (eau glycolée)
 rho2=1040.0 # masse volumique de l'eau glycolée
 m2=rho2*q2 # débit massique de l'eau glycolée
 Cp2=3942.0 # capacité calorifique massique de l'eau glycolée
+"""
+
+"""
+massif de stockage
+"""
 m_sable=70200.0 # masse de sable en kg
-Cp_sable=1470.0 # capacité calorifique massqieu du sable en J/Kg.K
+Cp_sable=1470.0 # capacité calorifique massique du sable en J/Kg.K
+
+"""
+paramètres dimensionnant le système géothermique qui équipera le stockage
+"""
 R1=0.0102 # Rayon intérieur du tube
 R2=0.0125 # Rayon extérieur 
-L_tube=5 # Longueur tube
-N_tube=16  # Nombre de tube
-Nu=4.36 #Nombre de tube
+L_tube=5 # Longueur tube en m
+N_tube=16  # Nombre de tubes
 lambda2=15.8
 lambda_tube=1.32
 Rcond=np.log(R2/R1)/(2*math.pi*N_tube*L_tube*lambda_tube)
 Rconv=1/(math.pi*N_tube*L_tube*lambda2)
+# unités ?
 k_global=1/(Rcond+Rconv)
+
+print("le k du système géothermique vaut {}".format(k_global))
+
+# pas utilisé ?
+Nu=4.36 #Nombre de tube
+
+
+# efficacité de l'échangeur
+eff = 0.8
+# débits dans chacune des parties de l'échangeur
+qdro = 0.035/3600 # m3/s
+qsto = qdro
+# rho_eau en provenance du fichier constantes est expérimée en kg/m3
+# Cpf en provenance du fichier constantes est exprimée en J/(kg.K)
+mdro = qdro * rho_eau
+msto = qsto * rho_eau
+cpdro = Cpf
+cpsto = Cpf
+
+k = k_global
+    
+coeff = (mdro * cpdro) / (msto * cpsto)
+
+print((msto * cpsto - k/2) / (msto * cpsto + k/2)) 
+    
+a = 1 - coeff * eff - (msto * cpsto - k/2) / (msto * cpsto + k/2)
+
+print("coeff vaut {} et a vaut {}".format(coeff, a))
+
+"""
 m2=1000*q2
 C2=4180.0 
 Tsol=10.0
 e_iso=0.001
 S_iso=2*math.pi*2*R2*N_tube*L_tube
-
+"""
 
 """
 SUMMER SIMULATION
@@ -99,47 +130,25 @@ i_summerEnd=i_summerStart+(summerEnd-summerStart)//step
 print(i_summerStart)
 print(i_summerEnd)
 input("press any key")
-for i in range (i_summerStart,i_summerEnd):
-
-    # Températures fluides sortant de l'échangeur vers le stockage
-    """
-    1 = SC : source chaude = fournie par le dromotherme
-    
-    2 = SF : source froide = fournie par les sondes ou les corbeilles
-    
-    Ts1_ech: Température de sortie de SC dromothermique après passage dans l'échangeur de séparation de réseau
-    
-    Te2_ech: Température d'entrée de SF, du stockage vers l'échangeur de séparation de réseau = Ts1_sto ?
-    
-    Ts2_ech: Température de sortie de SF, de l'échangeur de séparation de réseau vers le stockage
-    
-    """
-    Ts1_ech[i],Ts2_ech[i] = Tsorties_echangeur(Tsf_dro[i], Ts1_sto[i], rho_eau*qf, m1, Cpf, Cpf, eff)
-    
-    """
-    
-    désormais il faut calculer la température du stockage, que tu appelles Tsable
-    
-    comment fait-on là ? 
-    
-    ???
-    """
-    
-    
-plt.subplot(211)
-plt.plot(Tsf_dro,label="Te1",color="red")
-plt.plot(Ts1_ech,label="Ts1",color="purple")
-plt.legend()
-plt.subplot(212)
-plt.plot(Ts1_sto,label="Te2",color="blue")
-plt.plot(Ts2_ech,label="Ts2",color="orange")
-plt.legend()
-plt.show()
     
 
 #Température du stockage
-Tsable = odeint(F,10,meteo[0:-10,0])
+#Tsable = odeint(F,10,meteo[0:-10,0])
+Tsable = odeint(F,10,meteo[i_summerStart:i_summerEnd,0])
 
+plt.subplot(211)
+plt.plot(Tsor_dro,label="Tsor_dro",color="red")
+plt.plot(Tinj_dro,label="Tinj_dro",color="purple")
+plt.legend()
+plt.subplot(212)
+
+plt.plot(Tinj_sto,label="Tinj_sto",color="orange")
+plt.plot(Tsor_sto,label="Tsor_sto",color="blue")
+
+plt.plot(Tsable,label="Tsable",color="red")
+
+plt.legend()
+plt.show()
 
 
 """
