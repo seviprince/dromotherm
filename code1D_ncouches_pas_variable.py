9c6f921dd99ce17585caafb731ec52a4aa907716
alexandrecuer <alexandre.cuer@cerema.fr>
2020-05-31 10:40:57 +0200

    Update code1D_ncouches_pas_variable.py
    tenseur T
    axe 0 : Temps
    axe 1 : nombre de couches ou z
    axe 2 : nombre de blocs ou x

--- code1D_ncouches_pas_variable.py
+++ code1D_ncouches_pas_variable.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+path="examples/mod1D"
+
 #IMPORTATION DES DONNEES DES COUCHES DE CHAUSSEE
-input = np.loadtxt('input.txt')
+input = np.loadtxt('{}/input.txt'.format(path))
 nc = input.shape[0] # nombre de couches
 ha = input[:,0] # hauteur des couches
 le = input[:,1] # coef d'echanges des couches (derniere valeur non utilisee)
@@ -11,11 +13,11 @@ rc = input[:,2] # capacites calorifiques des couches
 kelvin = 273.15
 
 #IMPORTATION DES TEMPERATURES DU MODELE 2D
-T2d = kelvin + np.loadtxt('T2d2.txt')
+T2d = kelvin + np.loadtxt('{}/T2d2.txt'.format(path))
 
 
 albedo = 0.08
-eps = 0.92 
+epsilon = 0.92 
 sigma = 5.67e-8
 
 #IMPORTATION DES DONNES METEOS (VARIABLES EN FONCTION DU TEMPS)
@@ -26,21 +28,23 @@ sigma = 5.67e-8
 #5 : vitesse du vent (en m/s)
 #6 : rayonnement global (en W/m2)
 #7 : rayonnement atmospherique (en W/m2)
-meteo = np.loadtxt('meteo2.txt')
+meteo = np.loadtxt('{}./meteo2.txt'.format(path))
 f2 = 1000.0*1.1*(0.0036*meteo[:,4]+0.00423)   
 f1 = (1.0-albedo)*meteo[:,5] + meteo[:,6] + f2*(meteo[:,1]+kelvin)
-print f1
+print(f1)
 t = meteo[:,0]
 
 # MAILLAGE X
 #x = np.loadtxt('maillage_x.txt')
+#x = np.array([0,0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,3.25,3.50,3.75,4])
 x = np.array([0,0.75,1.5,2.25,3.0,3.75])
-nx = 10
-dx = 1.2*np.ones((nx))
+#nx = 10
+nx = x.shape[0]-1
+dx = 0.75*np.ones((nx))
 #nx = x.shape[0] # nombre de points en x
 #L = x[nx-1]
 #dx = x[1:]-x[0:nx-1]
-print dx
+print(dx)
 
 #L = 4.0 # Largeur de chaussee
 #dx = 0.75 # pas d'espace
@@ -53,11 +57,16 @@ dt = 3600.0
 #nx = int(L/dx) # Nombre de blocs
 nt = t.size # le nomnre de points dans la discretisation temporelle
 
-T = np.zeros((nc,nx,nt)) # Champ de temperature
+"""
+axe 0 : Temps
+axe 1 : nombre de couches ou z
+axe 2 : nombre de blocs ou x
+"""
+T = np.zeros((nt,nc,nx)) # Champ de temperature
 
 # Conditions initiales
 for i in range(nx) :
-    T[:,i,0] = T2d[0,1:]
+    T[0,:,i] = T2d[0,1:]
 
 # Conditions aux limites
 Tinj = 10.0 + kelvin
@@ -73,7 +82,7 @@ A[1:nc-1] = dt * (le[0:nc-2] + le[1:nc-1])
 A[nc-1] = dt * le[nc-2]
 #A[1] = A[1] + dt * (qf * Cf) / dx 
 A = A + ha*rc
-print A
+print(A)
 B[0:nc-1] = - dt * le[0:nc-1]  
 C[1:nc] = - dt * le[0:nc-1]
 
@@ -96,10 +105,10 @@ def sol_tridiag(A,B,C,D):
 
 
 for n in range(1,nt):
-    for j in range(0,nx):             
-        A[0] = dt * (f2[n] + le[0] + 4.0*eps*sigma*T[0,j,n-1]**3) + ha[0] * rc[0]
-        R = ha*rc*T[:,j,n-1]
-        R[0] = R[0] + dt * (f1[n] + 3.0*eps*sigma*T[0,j,n-1]**4)
+    for j in range(0,nx):  
+        A[0] = dt * (f2[n] + le[0] + 4.0*epsilon*sigma*T[n-1,0,j]**3) + ha[0] * rc[0]
+        R = ha*rc*T[n-1,:,j]
+        R[0] = R[0] + dt * (f1[n] + 3.0*epsilon*sigma*T[n-1,0,j]**4)    
         if j==0:
            #R[1] = R[1] - dt * lambd * Tinj
            R[1] = dt*10000000*Tinj
@@ -107,20 +116,19 @@ for n in range(1,nt):
            C[1] = 0.0
            B[1] = 0.0
         else:
-           R[1] = dx[j-1]*R[1] + dt * (qf * Cf) * (T[1,j-1,n-1]-T[1,j,n-1])
-           C[1] = - dx[j-1]*dt * le[0] 
-           B[1] = - dx[j-1]*dt * le[1]   
-           A[1] = dx[j-1]*dt * (le[0] + le[1]) + dx[j-1]*ha[1] * rc[1] #+ dt * (qf * Cf) / dx
-        T[:,j,n] = sol_tridiag(A,B,C,R)
+           R[1] = dx[j-1]*R[1] + dt * (qf * Cf) * (T[n-1,1,j-1]-T[n-1,1,j])
+           C[1] = - dx[j-1]*dt * le[0]
+           B[1] = - dx[j-1]*dt * le[1]
+           A[1] = dx[j-1]*dt * (le[0] + le[1]) + dx[j-1]*ha[1] * rc[1]
+        T[n,:,j] = sol_tridiag(A,B,C,R)
 
 
+np.savetxt('T1d.txt', T[:,:,-1]-kelvin, fmt='%.2e')
 
-
-np.savetxt('T0d.txt', T[:,nx-1,:]-kelvin, fmt='%.2e')    
-
-print T[0,nx-1,:]
-plt.plot(T[1,nx-1,:]-kelvin)
-plt.plot(T2d[:,2]-kelvin,'r-')
+print(T[0,nx-1,:])
+plt.plot(t,T[:,1,-1]-kelvin,label="1D model")
+plt.plot(t,T2d[:,2]-kelvin,'r-',label="T2d")
+plt.legend()
 plt.show()
         
     
