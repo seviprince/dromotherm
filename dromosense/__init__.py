27a7815411ecfc63cd7e0d7fc9138b402625412b
alexandrecuer <alexandre.cuer@cerema.fr>
2020-07-07 22:29:40 +0200

    Update __init__.py

--- dromosense/__init__.py
+++ dromosense/__init__.py
@@ -2,6 +2,7 @@ from .importation import getCsvDatas, getTxtModelicaDatas
 from .tools import *
 from .constantes import *
 
+# this is not necessary
 """
 __all__ = [ "getCsvDatas", "getTxtModelicaDatas",
             "rd",
