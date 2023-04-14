42264aeee6ed1dd93b6c2b1dd3232e0ac2dc4cef
seviprince <63263854+seviprince@users.noreply.github.com>
2023-04-14 14:40:42 +0200

    Create constantes.py

---
+++ examples/mod1D/dromosense/constantes/constantes.py
@@ -0,0 +1,132 @@
+
+albedo=0.08
+"""
+albedo
+
+grandeurs sans unité
+"""
+
+kelvin = 273.15
+"""
+
+zero °C expressed in K
+
+"""
+
+
+epsilon=0.92
+"""
+émissivité
+
+grandeurs sans unité
+"""
+
+ks=2.34
+"""
+conductivité thermique couche de surface
+
+unité : W/(m.K)
+"""
+
+
+kd=1.56
+"""
+conductivité thermique couche drainante
+
+unité : W/(m.K)
+"""
+
+
+kb=1.76
+"""
+conductivité thermique couche de base
+
+unité : W/(m.K)
+"""
+
+
+Cs=2144309
+"""
+capacité calorifique volumique couche de surface
+
+unité J/(m^3.K)
+"""
+
+
+Cd=1769723
+"""
+capacité calorifique volumique couche drainante
+
+unité J/(m^3.K)
+"""
+
+
+Cb=2676728
+"""
+capacité calorifique volumique couche de base
+
+unité J/(m^3.K)
+"""
+
+Cf=4181000
+"""
+capacité calorifique volumique de l'eau
+
+unité J/(m^3.K)
+"""
+
+
+sigma=5.67e-8
+"""
+constante de Stefan-Boltzmann en W/(m2K4)
+"""
+
+rho_eau=1000
+"""
+masse volumique de l'eau
+unité (kg/m^3)
+"""
+Cpf=4181
+"""
+capacité calorifique massique de l'eau
+
+unité J/(kg.K)
+"""
+Cpac=3942.0 
+"""
+Cpacité calorifique massique de l'eau glycolée
+
+unité (J/(m^3.K)
+       
+"""
+
+e_c=0.3
+"""
+épaisseur d'une couche de sable du stockage
+unité (m)
+"""
+"""
+Les position des différentes interaces des couches du stockage
+unité (m)
+"""
+Z0=0.0
+Zbe_i=0.2
+Zi_1=0.4
+Z1_2=Zi_1+e_c
+Z2_3=Z1_2+e_c
+Z3_4=Z2_3+e_c
+Z4_5=Z3_4+e_c
+Z5_i=Z4_5+e_c
+Zs_sol=Z5_i+0.2
+
+L=330000
+"""
+Chaleur latente de fusion de l'eau
+unité (J/kg)
+"""
+Tf=0
+
+""" 
+Température de fusion de l'eau
+unité (°C) 
+"""
